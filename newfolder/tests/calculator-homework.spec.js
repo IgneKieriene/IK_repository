2ec9a21d0e63e6e2236b716ac0c62bf4374b4736
Igne <Igne.kieriene@gmail.com>
2021-08-10 20:00:46 +0300

    homeworks after forEachFix

--- newfolder/tests/calculator-homework.spec.js
+++ newfolder/tests/calculator-homework.spec.js
@@ -1,48 +1,51 @@
 const { test, expect } = require('@playwright/test');
 //const { StartPage } = require('../BasicCalculator');
 
- const builds = ['0', '1', '2'];
- builds.forEach(build => {
- test(`Test_1_Button in ${build}`, async ({browser}) => {
-    const page = await browser.newPage();
-    await page.goto('https://testsheepnz.github.io/BasicCalculator');
-    const build = await page.isVisible('#selectBuild');
-    expect(build).toBe(true);
-    //const buildType = await page.$('#selectBuild');
-    await page.selectOption('#selectBuild', build);
-    const calculateButton = await page.isVisible('#calculateButton');
-    expect(calculateButton).toBe(true);
-  });
+
+const builds = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+builds.forEach(build => {
+test.only(`Test_1_Button in ${build}`, async ({browser}) => {
+const page = await browser.newPage(); 
+await page.goto('https://testsheepnz.github.io/BasicCalculator');
+const isBuildNowVisible = await page.isVisible('#selectBuild');  
+expect(isBuildNowVisible).toBe(true);
+await page.selectOption('#selectBuild', build);
+const calculateButton = await page.isVisible('#calculateButton');
+expect(calculateButton).toBe(true);
+});
 });
 
-  test.only('Test_2_ValidNumbers', async ({ page }) => {
-    await page.goto('https://testsheepnz.github.io/BasicCalculator');
-    const build = await page.isVisible('#selectBuild');
-    expect(build).toBe(true);
-    const buildType = await page.$('#selectBuild');
-    await buildType?.selectOption('0');
-    const firstNumber = await page.isVisible('#number1Field');
-    await page.click('#number1Field');
-    await page.fill('#number1Field', '@');
-    expect(firstNumber).toBe(true);
-    const secondNumber = await page.isVisible('#number2Field');
-    await page.click('#number2Field');
-    await page.fill('#number2Field', '4');
-    expect(secondNumber).toBe(true);
-    const operationButton = await page.isVisible('#selectOperationDropdown');
-    const subtract = await page.$('#selectOperationDropdown');
-    await subtract?.selectOption('1');
-    await page.click('#calculateButton');
-    const errorMessage = await page.textContent('#errorMsgField');
-    expect (errorMessage).toBe("Number 1 is not a number");
+  builds.forEach(build => {
+  test.only(`Test_2_Invalidnumbers in ${build}`, async ({browser}) => {
+  const page = await browser.newPage(); 
+  await page.goto('https://testsheepnz.github.io/BasicCalculator');
+  const isBuildNowVisible = await page.isVisible('#selectBuild');
+  expect(isBuildNowVisible).toBe(true);
+  await page.selectOption('#selectBuild', build);
+  const firstNumber = await page.isVisible('#number1Field');
+  await page.click('#number1Field');
+  await page.fill('#number1Field', '@');
+  expect(firstNumber).toBe(true);
+  const secondNumber = await page.isVisible('#number2Field');
+  await page.click('#number2Field');
+  await page.fill('#number2Field', '4');
+  expect(secondNumber).toBe(true);
+  const operationButton = await page.isVisible('#selectOperationDropdown');
+  const subtract = await page.$('#selectOperationDropdown');
+  await subtract?.selectOption('1');
+  await page.click('#calculateButton');
+  const errorMessage = await page.textContent('#errorMsgField');
+  expect (errorMessage).toBe("Number 1 is not a number");
 });
-  
-    test.only('Test_3_Concatenate', async ({ page }) => {
+});
+  builds.forEach(build => {
+    test.only(`Test_3_Concatenate in ${build}`, async ({browser}) => {
+    const page = await browser.newPage(); 
     await page.goto('https://testsheepnz.github.io/BasicCalculator');
-    const build = await page.isVisible('#selectBuild');
-    expect(build).toBe(true);
-    const buildType = await page.$('#selectBuild');
-    await buildType?.selectOption('0');
+    const isBuildNowVisible = await page.isVisible('#selectBuild');
+    expect(isBuildNowVisible).toBe(true);
+    await page.selectOption('#selectBuild', build);
     const firstNumber = await page.isVisible('#number1Field');
     await page.click('#number1Field');
     await page.fill('#number1Field', '5');
@@ -59,24 +62,26 @@ const { test, expect } = require('@playwright/test');
     //const answerField = await page.getAttribute('#numberAnswerField','value');
    const answerField = await page.inputValue('#numberAnswerField');
     expect (answerField).toBe("54");
-    
+});    
 });
-test.only('Test_4_Checkbox', async ({ page }) => {
+    builds.forEach(build => {
+    test.only(`Test_4_Checkbox in ${build}`, async ({browser}) => {
+    const page = await browser.newPage(); 
     await page.goto('https://testsheepnz.github.io/BasicCalculator');
-    const build = await page.isVisible('#selectBuild');
-    expect(build).toBe(true);
-    const buildType = await page.$('#selectBuild');
-    await buildType?.selectOption('0');
+    const isBuildNowVisible = await page.isVisible('#selectBuild');
+    expect(isBuildNowVisible).toBe(true);
+    await page.selectOption('#selectBuild', build);
     const integersOption = expect(await page.isChecked('#integerSelect')).toBeFalsy();
- 
+});
 });
 
-test.only('Test_5_DivideFrom0', async ({ page }) => {
+    builds.forEach(build => {
+    test.only(`Test_5_DivideFrom0 in ${build}`, async ({browser}) => {
+    const page = await browser.newPage(); 
     await page.goto('https://testsheepnz.github.io/BasicCalculator');
-    const build = await page.isVisible('#selectBuild');
-    expect(build).toBe(true);
-    const buildType = await page.$('#selectBuild');
-    await buildType?.selectOption('0');
+    const isBuildNowVisible = await page.isVisible('#selectBuild');
+    expect(isBuildNowVisible).toBe(true);
+    await page.selectOption('#selectBuild', build);
     const firstNumber = await page.isVisible('#number1Field');
     await page.click('#number1Field');
     await page.fill('#number1Field', '10');
@@ -91,6 +96,7 @@ test.only('Test_5_DivideFrom0', async ({ page }) => {
     await page.click('#calculateButton');
     const errorMessage = await page.textContent('#errorMsgField');
     expect (errorMessage).toBe("Divide by zero error!");
+});
 });
 
 
